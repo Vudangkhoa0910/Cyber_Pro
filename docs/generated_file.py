bdd6f99f6e60615f3e80d78276d92be352fa8eac
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-09-21 11:19:57 +0700

    Add gene_file v1

---
+++ docs/generated_file.py
@@ -0,0 +1,11 @@
+test_content = """
+KhoaCyber Encryption Test
+This is a sample text that will be encrypted using Caesar Cipher with a shift of 3.
+"""
+
+# Write the content to a file
+file_path = '/Users/vudangkhoa/Documents/CODE ON CLASS/An toàn và bảo mật thông tin/CodePractice/docs/test_input.txt'
+with open(file_path, 'w') as test_file:
+    test_file.write(test_content)
+
+file_path 
